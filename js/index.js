c39fa57eb60c0d2b9c1e12853da05d0090d53c99
azizulwd <azizulislamgd@gmail.com>
2021-08-25 00:45:53 +0600

    section style function added

--- js/index.js
+++ js/index.js
@@ -3,8 +3,19 @@ for(let allh2Element of allH5){
     allh2Element.style.color = "lightblue";
 }
 
-// backpack section
-let backpackBg = document.getElementById("backpack");
-backpackBg.style.backgroundColor = "tomato";
-backpackBg.style.padding = "20px";
-backpackBg.style.borderRadius = "15px";
+let card = document.querySelectorAll(".card");
+for(let cards of card){
+    cards.style.borderRadius = "30px";
+}
+
+
+// product section function
+function productDesign(sectionId){
+    let sectionStyle = document.getElementById(sectionId);
+    sectionStyle.style.backgroundColor = "tomato";
+    sectionStyle.style.padding = "20px";
+    sectionStyle.style.borderRadius = "15px";
+}
+
+productDesign("backpack");
+productDesign("shoes");
