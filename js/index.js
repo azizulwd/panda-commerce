73fe553525c5a07ec8b6c725199059ad0eba142e
azizulwd <azizulislamgd@gmail.com>
2021-08-25 01:58:20 +0600

    update some functionality

--- js/index.js
+++ js/index.js
@@ -19,3 +19,52 @@ function productDesign(sectionId){
 
 productDesign("backpack");
 productDesign("shoes");
+
+
+// remove button
+let removeBtn = document.querySelectorAll(".buy_now_btn");
+for(let removeButton of removeBtn){
+    removeButton.addEventListener("click", function(){
+        removeButton.style.display = "none";
+    });
+}
+
+
+// button active or disabled
+let emailInput = document.getElementById("email_input");
+let submitBtn = document.getElementById("submit_btn");
+
+emailInput.addEventListener("keyup", function(){
+    if(emailInput.value == "email"){
+        submitBtn.removeAttribute("disabled");
+    }
+    else{
+        submitBtn.setAttribute("submitBtn");
+    }
+});
+
+
+// subscribe bg change
+let subscribe = document.getElementById("subscribe");
+subscribe.addEventListener("dblclick", function(){
+    subscribe.style.backgroundColor = "orange";
+});
+
+
+
+
+
+// Changing image
+let changeImgOne = document.getElementById("shoe_one");
+let changeImgTwo = document.getElementById("shoe_two");
+let changeImgThree = document.getElementById("shoe_three");
+
+function changeImages(imageId, imageSrc){
+    imageId.addEventListener("mouseenter", function(){
+        imageId.src = imageSrc;
+    });
+}
+
+changeImages(changeImgOne, "images/shoes/shoe-3.png");
+changeImages(changeImgTwo, "images/shoes/shoe-1.png");
+changeImages(changeImgThree, "images/shoes/shoe-2.png");
