ed2d7ec6f2e499f736247d7754a2f81ef3a10fc2
azizulwd <azizulislamgd@gmail.com>
2021-08-25 00:37:26 +0600

    backpack section design update with javascript

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ let allH5 = document.querySelectorAll("h5");
 for(let allh2Element of allH5){
     allh2Element.style.color = "lightblue";
 }
+
+// backpack section
+let backpackBg = document.getElementById("backpack");
+backpackBg.style.backgroundColor = "tomato";
+backpackBg.style.padding = "20px";
+backpackBg.style.borderRadius = "15px";
